0436ab7053c41d6954b2a50da2013b34fb7f2d6e
Juan Palma <juan.palma.alvarez@gmail.com>
2025-04-27 16:51:55 -0400

    create fighters

---
+++ src/consts/fighters.ts
@@ -0,0 +1,58 @@
+export const FIGHTERS = [
+    {
+        id: "abby",
+        name: "Abby"
+    },
+    {
+        id: "alana",
+        name: "Alana"
+    },
+    {
+        id: "andoni",
+        name: "Andoni"
+    },
+    {
+        id: "arigeli",
+        name: "Arigeli"
+    },
+    {
+        id: "carlos",
+        name: "Carlos"
+    },
+    {
+        id: "gaspi",
+        name: "Gaspi"
+    },
+    {
+        id: "grefg",
+        name: "Grefg"
+    },
+    {
+        id: "peereira",
+        name: "Peereira"
+    },
+    {
+        id: "perxitaa",
+        name: "Perxitaa"
+    },
+    {
+        id: "rivaldios",
+        name: "Rivaldios"
+    },
+    {
+        id: "roro",
+        name: "Roro"
+    },
+    {
+        id: "tomas",
+        name: "Tomas"
+    },
+    {
+        id: "viruzz",
+        name: "Viruzz"
+    },
+    {
+        id: "westcol",
+        name: "Westcol"
+    }
+]
