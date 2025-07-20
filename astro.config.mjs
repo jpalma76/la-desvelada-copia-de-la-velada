2a37eef5655344c3a1338bf60e0133f143cca0e7
Juan Palma <juan.palma.alvarez@gmail.com>
2025-07-19 20:24:28 -0400

    fixes image re-rendering error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   output: 'server',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  build: {
+    inlineStylesheets: 'always',
+  },
+
+  adapter: netlify(),
+  site: 'https://la-desvelada-v-sitio-no-oficial.netlify.app/'
+});
