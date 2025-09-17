b0366783cb7fcffe3e690460b624517a1a57f2bc
Juan Palma <juan.palma.alvarez@gmail.com>
2025-09-16 22:59:30 -0300

    add tltle

--- src/consts/combats.ts
+++ src/consts/combats.ts
@@ -1,36 +1,43 @@
 export const COMBATS = [
     {
         id: 1,
+        title: "Pereira vs Rivaldios",
         fighter1: "peereira",
         fighter2: "rivaldios"
     },
     {
         id: 2,
+        title: "Perxitaa vs Gaspi",
         fighter1: "perxitaa",
         fighter2: "gaspi"
     },
     {
         id: 3,
+        title: "Abby vs Roro",
         fighter1: "abby",
         fighter2: "roro"
     },
     {
         id: 4,
+        title: "Andoni vs Carlos",
         fighter1: "andoni",
         fighter2: "carlos"
     },
     {
         id: 5,
+        title: "Viruzz vs Tomas",
         fighter1: "viruzz",
         fighter2: "tomas"
     },
     {
         id: 6,
+        title: "Alana vs Arigeli",
         fighter1: "alana",
         fighter2: "arigeli"
     },
     {
         id: 7,
+        title: "Grefg vs Westcol",
         fighter1: "grefg",
         fighter2: "westcol"
     }
